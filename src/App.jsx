c45ab57a14fb499cfc1b013302735f4cb4a51f3e
PatS4050 <patrick@straatman.eu>
2026-05-06 16:02:30 +0200

    counter aangemaakt en array met objecten als inventory aangemaakt

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 
+import react from "react";
 import './App.css'
+import {fruits} from "./constants/inventory.js";
 
 function App() {
+    // const [fruit, setfruit] = react.useState ("")
+    const [counter, setCounter] = react.useState (0);
+    const countIncrement = () => setCounter(prevCount => prevCount + 1);
+    const countDecrement = () => setCounter(prevCount => prevCount - 1);
 
   return (
     <>
         <h1>Fruitmand bezorgservice</h1>
+        <section>
+            <p>{(fruits[0].type)}
+            <button onClick={countDecrement}>-</button>
+            <span>{counter}</span>
+            <button onClick={countIncrement}>+</button>
+            </p>
+            <p>{(fruits[1].type)}
+                <button onClick={countDecrement}>-</button>
+                <span>{counter}</span>
+                <button onClick={countIncrement}>+</button>
+            </p>
+        </section>
     </>
   )
 }
---
+++ src/constants/inventory.js
@@ -0,0 +1,19 @@
+// Alle producten uit de winkel //
+export let fruits = [
+    {
+        type: 'Aardbeien',
+        sourceImage: '',
+    },
+    {
+        type: 'Bananen',
+        sourceImage: '',
+    },
+    {
+        type: 'Appels',
+        sourceImage: '',
+    },
+    {
+        type: 'Kiwis',
+        sourceImage: '',
+    },
+];
